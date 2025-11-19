a18d0f280281452f89b97e81ee35babcd75b4d78
pprtbia <biancarb1421@gmail.com>
2025-11-18 21:37:29 -0300

    Feat - Evento do botão 'Agendar Consulta' e LocalStorage

--- miau_dota-app/src/details-mj.js
+++ miau_dota-app/src/details-mj.js
@@ -11,12 +11,41 @@ const vet = vets.find(a => a.id == id_vet)
 
 if (vet) {
     document.getElementById('vet-image').src = vet.image
-    // document.getElementById('vet-imagem').alt = vet.nome;
     document.getElementById('vet-name').textContent = vet.name;
     document.getElementById('vet-desc').textContent = vet.description;
     document.getElementById('vet-specialty').textContent = vet.specialty
     document.getElementById('vet-location').textContent = vet.location;
   } else {
-    document.body.innerHTML = '<h1 class="vet-não-encontrado">vet não encontrado!</h1>';
+    document.body.innerHTML = '<h1 style="color: black;" class="vet-não-encontrado">Veterinário não encontrado!</h1>';
   }
 
+//---------------------------------------botão de consulta-------------------------------------------------//
+
+const btn = document.getElementById("btnAgendar");
+
+let marcado = localStorage.getItem("Agendar") === "true";
+
+atualizarbotao();
+
+btn.addEventListener("click", () => {
+
+  // Ele vai alterar de true/false
+  marcado = !marcado;
+
+  // Vao salvar a mudança no localStorage
+  localStorage.setItem("Agendar", marcado)
+
+  // Vai atualizar o botao
+  atualizarbotao();
+});
+
+function atualizarbotao() {
+  if(marcado){
+    btn.textContent = "Desmarcar Consulta";
+    btn.style.backgroundColor = "teal";
+  }
+  else {
+    btn.textContent = "Agendar Consulta";
+    btn.style.backgroundColor = "";
+  }
+}
